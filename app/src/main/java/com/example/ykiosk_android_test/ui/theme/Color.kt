1f18889f7a89b02b834c5596ae10274e0c9aef28
Yoon <01csyoon@gmail.com>
2025-12-19 15:03:44 +0900

    feat: 텍스트 설정, 색상 설정
    color의 경우 임시로 몇 개 만들어둠. ui 컨셉이 잡히면 향후 수정

---
+++ app/src/main/java/com/example/ykiosk_android_test/ui/theme/Color.kt
@@ -0,0 +1,32 @@
+package com.example.ykiosk_android_test.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val DawnMist = Color(0xFFbbcddc)
+
+val Dark1 = Color(0xFF030303)
+val Dark2 = Color(0xFF030303)
+val Dark3 = Color(0xDD111111)
+val Gray = Color(0xFFC2C2C2)
+val GraySemiLight = Color(0xFFe0e0e0)
+val GrayLight = Color(0xFFeeeeee)
+
+val WhiteSemiLight = Color(0xEEFDFFFF)
+
+val BackgroundColorWhite = Color(0xFFFAFAFA)
+
+val DefaultBackgroundGray = Color(0xFFCFCFCF)
+
+val ChoicedBackground = Color(0xFF6F6F6F)
+
+val MenuBackgroundWhite = Color(0xFFF3F3F3)
+val MenuAreaBackgroundWhite = Color(0xFFE8E8e8)
+
