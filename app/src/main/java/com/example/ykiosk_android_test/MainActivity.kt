c4a1635080fb9e52848f38129cc8eb318b4c1023
Yoon <01csyoon@gmail.com>
2025-12-27 21:27:27 +0900

    feat: 앱 시작시 블루투스 권한 확인

--- app/src/main/java/com/example/ykiosk_android_test/MainActivity.kt
+++ app/src/main/java/com/example/ykiosk_android_test/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ykiosk_android_test
 
+import android.app.Activity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,13 +14,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.ykiosk_android_test.basic.MyApp
 import com.example.ykiosk_android_test.basic.StartScreen
+import com.example.ykiosk_android_test.bluetooth.BluetoothPermissionHandler
 import com.example.ykiosk_android_test.navigation.MainAppNavHost
 import com.example.ykiosk_android_test.order.OrderScreen
 import com.example.ykiosk_android_test.ui.theme.Ykiosk_android_testTheme
@@ -41,13 +49,31 @@ class MainActivity : ComponentActivity() {
 //            }
 //        }
         setContent {
-            val windowSizeClass = calculateWindowSizeClass(this)
-            val widthSizeClass = windowSizeClass.widthSizeClass
-            
-            Ykiosk_android_testTheme {
-                MainAppNavHost()
-                //MyApp()
-                //StartScreen()
+            val context = LocalContext.current
+            val activity = context as? Activity
+            var hasPermission by remember { mutableStateOf(false) }
+
+            if (!hasPermission) {
+                // 권한이 없으면 요청 화면 실행
+                BluetoothPermissionHandler(
+                    onPermissionsGranted = { hasPermission = true },
+                    onPermissionsDenied = {
+                        Toast.makeText(context, "블루투스 권한이 거부되어 앱을 종료합니다.", Toast.LENGTH_LONG).show()
+                        //권한이 없으니 앱 종료
+                        activity?.finish()
+                    }
+                )
+            } else {
+                // 권한 허용 후 실제 앱 UI (2단계에서 만들 기기 리스트 화면)
+
+                val windowSizeClass = calculateWindowSizeClass(this)
+                val widthSizeClass = windowSizeClass.widthSizeClass
+
+                Ykiosk_android_testTheme {
+                    MainAppNavHost()
+                    //MyApp()
+                    //StartScreen()
+                }
             }
         }
     }
