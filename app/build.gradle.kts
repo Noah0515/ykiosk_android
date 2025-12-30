90294c361eaf6b1fcd6059610ae12c91b7da67a3
Yoon <01csyoon@gmail.com>
2025-12-31 05:53:52 +0900

    feat: 각종 gradle 설정
    카카오, 여러 라이브러리 등

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
         compose = true
     }
 }
+val LATEST_VERSION = "2.20.0"
 
 dependencies {
 
@@ -63,4 +64,28 @@ dependencies {
     //implementation("androidx.navigation3:navigation-core:1.") // 예시 버전
     //implementation("androidx.navigation3:navigation-compose:1.1.0") // 예시 버전
 
+    implementation("com.kakao.sdk:v2-all:${LATEST_VERSION}")
+// 전체 모듈 추가, 2.11.0 버전부터 지원
+    implementation("com.kakao.sdk:v2-user:${LATEST_VERSION}")
+// 카카오 로그인 API 모듈
+    implementation("com.kakao.sdk:v2-share:${LATEST_VERSION}")
+// 카카오톡 공유 API 모듈
+    implementation("com.kakao.sdk:v2-talk:${LATEST_VERSION}")
+// 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation("com.kakao.sdk:v2-friend:${LATEST_VERSION}")
+// 피커 API 모듈
+    implementation("com.kakao.sdk:v2-navi:${LATEST_VERSION}")
+// 카카오내비 API 모듈
+    implementation("com.kakao.sdk:v2-cert:${LATEST_VERSION}")
+// 카카오톡 인증 서비스 API 모듈
+
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
 }
