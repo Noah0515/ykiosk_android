15a0d0c5d0513dbb57c555ede6400c11dcefb141
Yoon <01csyoon@gmail.com>
2025-12-30 23:11:43 +0900

    feat: 프린터 테스트 url

--- app/src/main/java/com/example/ykiosk_android_test/navigation/Route.kt
+++ app/src/main/java/com/example/ykiosk_android_test/navigation/Route.kt
@@ -6,6 +6,7 @@ object NavRoute {
     const val ORDER = "order_screen" // 주문 화면
     const val PRINTER_LIST = "printer_list_screen" //프린터 목록 화면
     //파라미터 있는 경로
+    const val PRINT_CONTROL = "print_control_screen/{address}/{name}"
     const val ORDER_COMPLETE = "order_complete_screen/{storeId}?data={orderData}" // 가게id와 주문정보(json)을 보냄
 
     fun orderCompletePath(storeId : String, orderData : String) : String {// json으로 전달. 근데 json은 못가서 string으로 보내고 parsing
