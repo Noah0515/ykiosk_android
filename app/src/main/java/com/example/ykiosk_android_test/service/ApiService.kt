f38755f95c61c43f85a293686a36bd3a1f27c1f0
Yoon <01csyoon@gmail.com>
2025-12-31 05:59:50 +0900

    feat: 가게 메뉴정보 받아올 api 설정

--- app/src/main/java/com/example/ykiosk_android_test/service/ApiService.kt
+++ app/src/main/java/com/example/ykiosk_android_test/service/ApiService.kt
@@ -1,9 +1,12 @@
 package com.example.ykiosk_android_test.service
 
+import com.example.ykiosk_android_test.DTO.response.StoreMenuDetailResponse
 import com.example.ykiosk_android_test.DTO.response.StoreResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/user/store/list")
@@ -12,4 +15,9 @@ interface ApiService {
     //대충 post용은 이렇게 생김
     @POST("api 주소")
     fun postmethod(): Call<Int>
+
+    @GET("api/user/store/menu-list")
+    suspend fun getStoreMenuList(
+        @Query("storeId") storeId : String
+    ): Response<StoreMenuDetailResponse>
 }
