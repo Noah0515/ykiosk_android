9a5e0f8909cd57b1692f576b81e7b0662cea8af2
Yoon <01csyoon@gmail.com>
2025-12-31 10:56:46 +0900

    feat: 주문내역 post용 api

--- app/src/main/java/com/example/ykiosk_android_test/service/ApiService.kt
+++ app/src/main/java/com/example/ykiosk_android_test/service/ApiService.kt
@@ -1,9 +1,11 @@
 package com.example.ykiosk_android_test.service
 
+import com.example.ykiosk_android_test.DTO.request.OrderRequest
 import com.example.ykiosk_android_test.DTO.response.StoreMenuDetailResponse
 import com.example.ykiosk_android_test.DTO.response.StoreResponse
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -20,4 +22,9 @@ interface ApiService {
     suspend fun getStoreMenuList(
         @Query("storeId") storeId : String
     ): Response<StoreMenuDetailResponse>
+
+    @POST("api/user/store/menu/new-order")
+    suspend fun postOrder(
+        @Body orderRequest: OrderRequest // 우리가 만든 DTO 객체
+    ): Response<Int> // 성공/실패 응답 (Unit은 내용물이 없을 때 사용)
 }
